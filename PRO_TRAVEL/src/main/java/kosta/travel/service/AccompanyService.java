92278e261cccf9482cdc2be7b9c36a783debd2c1
kosta-PC\kosta <zzzsun09@naver.com>
2017-06-02 18:53:15 +0900

    routelist class 추가 by su

--- PRO_TRAVEL/src/main/java/kosta/travel/service/AccompanyService.java
+++ PRO_TRAVEL/src/main/java/kosta/travel/service/AccompanyService.java
@@ -1,29 +1,48 @@
 package kosta.travel.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kosta.travel.domain.AccompanyVO;
+import kosta.travel.domain.RouteList;
 import kosta.travel.domain.RouteVO;
 import kosta.travel.persistence.AccompanyDao;
 
 @Service
 public class AccompanyService {
-
+	
 	@Inject
 	private AccompanyDao dao;
+	
+	private RouteVO route;
+	
 	
 	public List<AccompanyVO> getUserRoute(int id) throws Exception {
 		return dao.getUserRoute(id);
 	}
 
-	public int insertRoute(RouteVO route)throws Exception{
-		/*  Object obj = JSONValue.parseWithException(json);
+	@ResponseBody
+	public void insertRoute()throws Exception{
+		/*RouteDao dao = RouteDao.getInstance();
+		
+		String id =(String)request.getSession().getAttribute("member_id");
+		String json=request.getParameter("json");
+		System.out.println(json);
+		//for(int i=0;i<jsonArray.size())
+		//jsonArray.add(index, value);
+		  
+	  	  Object obj = JSONValue.parseWithException(json);
 	      JSONArray jsonArray = (JSONArray)obj;
 	      
 	      for(int i=0;i<jsonArray.size();i++) {
@@ -36,10 +55,11 @@ public class AccompanyService {
 	    	  route.setCor_region(rstJson.get("title").toString());
 	    	  //route.setTp_date(rstJson.get("date").toString());
 	    	  route.setTp_date(rstJson.get("eventdate").toString());
-	    	  dao.insertRoute(route);*/
-	    	  
-		return 0;
+	    	  dao.insertRoute(route);
+	      }
+		
+		
 	}
-	
+	*/
 	
 }
