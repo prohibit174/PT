4e27bb38bb7a546713fa7aa261eabd4b6cd1021b
김동욱 <ponic2@naver.com>
2017-06-12 18:02:46 +0900

    auth인터셉터랑 짜잘한거 좀 만짐

--- PRO_TRAVEL/src/main/webapp/resources/js/login_success.js
+++ PRO_TRAVEL/src/main/webapp/resources/js/login_success.js
@@ -1,13 +1,15 @@
-$(function(){
-	if($("span.session").text() != "null"){
-		console.log($("span.session").text());
-		$("li.welcome").show();
-		$("li#mypage").show();
-		$("#login-button").text("LOGOUT");
-	}else if($('#login-button').text() === "LOGOUT"){
-		$('#login-button').text("LOGIN");
-		$('li.welcome').hide();
-		$("li#mypage").hide();
-	}
-	
+$(function() {
+	$("#login-button").on("click", function() {
+		if ($("span.session").text() != "null") {
+			console.log($("span.session").text());
+			$("li.welcome").show();
+			$("li#mypage").show();
+			$("#login-button").text("LOGOUT");
+		} else if ($('#login-button').text() === "LOGOUT") {
+			$('#login-button').text("LOGIN");
+			$('li.welcome').hide();
+			$("li#mypage").hide();
+		}
+
+	});
 });
