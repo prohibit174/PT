2f02313be6d0c50290e4d72f36904723367b9056
kosta <kosta@kosta-PC>
2017-06-12 18:44:29 +0900

    doing admin login

--- PRO_TRAVEL/src/main/java/kosta/travel/controller/AdminController.java
+++ PRO_TRAVEL/src/main/java/kosta/travel/controller/AdminController.java
@@ -1,10 +1,15 @@
 package kosta.travel.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
 
-	
+	@RequestMapping("/adminLogin")
+	public String test_admin(){
+		return "/mypage_admin/test_admin";
+	}
 		
 }
