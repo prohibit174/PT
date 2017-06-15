a58e770edf766bdf9f298512235c55ebb1d30585
김동욱 <ponic2@naver.com>
2017-06-15 18:18:22 +0900

    쪽지 insert, list 완료

---
+++ PRO_TRAVEL/src/main/java/kosta/travel/persistence/MessageDAOImpl.java
@@ -0,0 +1,36 @@
+package kosta.travel.persistence;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import kosta.travel.domain.MessageVO;
+
+@Repository
+public class MessageDAOImpl implements MessageDAO {
+
+	@Inject
+	private SqlSession session;
+	
+	private static String namespace = 
+			"kosta.travel.mappers.messageMapper";
+	
+	@Override
+	public void message_insert(MessageVO vo) throws Exception {
+		session.insert(namespace+".message_insert", vo);
+	}
+
+	@Override
+	public Integer max_num() throws Exception {
+		return session.selectOne(namespace+".max_num");
+	}
+
+	@Override
+	public List<MessageVO> list() throws Exception {
+		return session.selectList(namespace + ".list");
+	}
+
+}
