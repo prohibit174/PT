2020f6d6de30912f99f7bf50a0f2a0425c68b753
바캐진 <gpwls2440@gmail.com>
2017-06-19 18:44:03 +0900

    혜진 블로그

--- PRO_TRAVEL/src/main/java/kosta/travel/domain/BlogVO.java
+++ PRO_TRAVEL/src/main/java/kosta/travel/domain/BlogVO.java
@@ -1,54 +1,79 @@
 package kosta.travel.domain;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class BlogVO {
-	private String b_id;
+	private int b_id;
 	private String b_title;
 	private String u_name;
-	private String b_instruction;
+	private String b_introdution;
 	private String u_img;
-	
+	private MultipartFile file2;
+	 
+	 
 	public BlogVO(){}
 
-	public String getB_id() {
+
+	public int getB_id() {
 		return b_id;
 	}
 
-	public void setB_id(String b_id) {
+
+	public void setB_id(int b_id) {
 		this.b_id = b_id;
 	}
+
 
 	public String getB_title() {
 		return b_title;
 	}
 
+
 	public void setB_title(String b_title) {
 		this.b_title = b_title;
 	}
+
 
 	public String getU_name() {
 		return u_name;
 	}
 
+
 	public void setU_name(String u_name) {
 		this.u_name = u_name;
 	}
 
+
 	public String getB_instruction() {
-		return b_instruction;
+		return b_introdution;
 	}
 
+
 	public void setB_instruction(String b_instruction) {
-		this.b_instruction = b_instruction;
+		this.b_introdution = b_introdution;
 	}
+
 
 	public String getU_img() {
 		return u_img;
 	}
 
+
 	public void setU_img(String u_img) {
 		this.u_img = u_img;
 	}
 
+
+	public MultipartFile getFile2() {
+		return file2;
+	}
+
+
+	public void setFile2(MultipartFile file2) {
+		this.file2 = file2;
+	}
+
+	
 	
 	
 	
