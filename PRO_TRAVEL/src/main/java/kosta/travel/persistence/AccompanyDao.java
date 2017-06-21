d69e599ca32be537765660d2e1b34848ac075f17
kosta-PC\kosta <zzzsun09@naver.com>
2017-06-21 10:00:21 +0900

    commit for checking by su

--- PRO_TRAVEL/src/main/java/kosta/travel/persistence/AccompanyDao.java
+++ PRO_TRAVEL/src/main/java/kosta/travel/persistence/AccompanyDao.java
@@ -14,87 +14,114 @@ import kosta.travel.domain.RouteVO;
 
 @Repository
 public class AccompanyDao {
-	
-	private static AccompanyDao dao = new AccompanyDao();
+   
+   private static AccompanyDao dao = new AccompanyDao();
 
-	
-	public static AccompanyDao getInstance() {
-		return dao;
-	}
+   
+   public static AccompanyDao getInstance() {
+      return dao;
+   }
 
-	@Inject
-	private SqlSessionTemplate session;
-	
-	
-	private static String namespace = "pro_travel.mapper.accompanyMapper";
-	
-	public List<AccompanyVO> getUserRoute(String id) throws Exception {
-		return session.selectList(namespace+".getUserRoute", id);
-	}
-	
-	public List<AccompanyVO> getUserInfo(String ID) throws Exception {
-		return session.selectList(namespace+".getUserInfo", ID);
-	}
+   @Inject
+   private SqlSessionTemplate session;
+   
+   
+   private static String namespace = "pro_travel.mapper.accompanyMapper";
+   
+   public List<AccompanyVO> getUserRoute(String id) throws Exception {
+      return session.selectList(namespace+".getUserRoute", id);
+   }
+   
+   public List<AccompanyVO> getUserInfo(String ID) throws Exception {
+      return session.selectList(namespace+".getUserInfo", ID);
+   }
 
-	public List<AccompanyVO> getAccompanies(AccompanyVO accompany) throws Exception {
-		return session.selectList(namespace+".getAccompanies", accompany);
-	}
-	
-	public void insertRoute(RouteVO route){
-		session.insert(namespace+".insertRoute", route);
-	}
-	
-	public List<AccompanyVO> getTraveler(RouteVO route){
-		return session.selectList(namespace+".getTraveler", route);
-	}
+   public List<AccompanyVO> getAccompanies(AccompanyVO accompany) throws Exception {
+      return session.selectList(namespace+".getAccompanies", accompany);
+   }
+   
+   public void insertRoute(RouteVO route){
+      session.insert(namespace+".insertRoute", route);
+   }
+   
+   public List<AccompanyVO> getTraveler(RouteVO route){
+      return session.selectList(namespace+".getTraveler", route);
+   }
 
-	   public List<CordinatesVO> getCordinates(){
-		      return session.selectList(namespace+".getCordinates");
-		   }
-	
-	   public void registGroup(GroupVO group){
-		   session.insert(namespace+".registGroup", group);
-	   }
-	   
-	   public String initGroup(){
-		   return session.selectOne(namespace+".initGroup");
-	   }
-	   
-	   public void registGroupZero(GroupVO group){
-		   session.insert(namespace+".registGroupZero", group);
-	   }
-	   
-	   public List<GroupVO> getGroupInfo1(String u_id) {
-			// TODO Auto-generated method stub
-			System.out.println("u_id : "+u_id); 
-			return session.selectList(namespace + ".getGroupInfo1", u_id);
-		}
+      public List<CordinatesVO> getCordinates(){
+            return session.selectList(namespace+".getCordinates");
+         }
+   
+      public void registGroup(GroupVO group){
+         session.insert(namespace+".registGroup", group);
+      }
+      
+      public String initGroup(){
+         return session.selectOne(namespace+".initGroup");
+      }
+      
+      public void registGroupZero(GroupVO group){
+         session.insert(namespace+".registGroupZero", group);
+      }
+      
+      public List<GroupVO> getGroupInfo1(String u_id) {
+         // TODO Auto-generated method stub
+         return session.selectList(namespace + ".getGroupInfo1", u_id);
+      }
 
-		public int getCurrent(GroupVO groupVO) {
-			// TODO Auto-generated method stub
-			return session.selectOne(namespace + ".getCurrent", groupVO);
-		}
-		
-		public List<GroupVO> searchGroupList(RouteVO route) {
-				return session.selectList(namespace + ".searchGroupList", route);
-	}
-	
-		public void requestGroup(GroupVO groupvo){
-			if(session.selectOne(namespace + ".getCurrentGroupRequestNum") == null){	
-				session.insert(namespace + ".requestGroupZero", groupvo);
-			}else{
-			session.insert(namespace + ".requestGroup", groupvo);
-			}
+      public int getCurrent(GroupVO groupVO) {
+         // TODO Auto-generated method stub
+         return session.selectOne(namespace + ".getCurrent", groupVO);
+      }
+
+      public List<GroupVO> getGroupInfo2(String u_id) {
+         System.out.println("u_id : "+u_id); 
+         return session.selectList(namespace + ".getGroupInfo2", u_id);
+      }
+
+      public int getCurrent2(GroupVO groupVO2) {
+         return session.selectOne(namespace + ".getCurrent2", groupVO2);
+      }
+
+
+      public GroupVO getGroupDetailInfo(int group_id) {
+         return session.selectOne(namespace + ".getGroupDetailInfo", group_id);
+
+      }
+
+      public List<GroupVO> getGuestGroupList(int group_id) {
+         return session.selectList(namespace + ".getGuestGroupList", group_id);
+      }
+
+      public void Accom_accept(int acr_num) {
+         session.insert(namespace+".Accom_accept", acr_num);         
+      }
+
+      public void Accom_reject(int acr_num) {
+         // TODO Auto-generated method stub
+         session.insert(namespace+".Accom_reject", acr_num);         
+
+      }
+      
+      public List<GroupVO> searchGroupList(RouteVO route) {
+            return session.selectList(namespace + ".searchGroupList", route);
+   }
+   
+      public void requestGroup(GroupVO groupvo){
+         if(session.selectOne(namespace + ".getCurrentGroupRequestNum") == null){   
+            session.insert(namespace + ".requestGroupZero", groupvo);
+         }else{
+         session.insert(namespace + ".requestGroup", groupvo);
+         }
 }
-		
-		
+      
+      
 }
 
 
 
-	
-	
-	
-	
-
+   
+   
+   
+   
 
--- PRO_TRAVEL/src/main/webapp/WEB-INF/views/mypage/mypage_accompCheck.jsp
+++ PRO_TRAVEL/src/main/webapp/WEB-INF/views/mypage/mypage_accompCheck.jsp
@@ -1,5 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+   pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <%@ include file="/WEB-INF/views/include/header.jsp"%>
@@ -15,64 +15,64 @@
 @CHARSET "EUC-KR";
 
 .wrapper {
-	margin: 0 auto;
-	padding-left: 30px;
-	max-width: 800px;
+   margin: 0 auto;
+   padding-left: 30px;
+   max-width: 800px;
 }
 
 .table {
-	margin: 0 0 40px 0;
-	width: 100%;
-	box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-	display: table;
+   margin: 0 0 40px 0;
+   width: 100%;
+   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+   display: table;
 }
 
 @media screen and (max-width: 580px) {
-	.table {
-		display: block;
-	}
+   .table {
+      display: block;
+   }
 }
 
 .row {
-	display: table-row;
-	background: #f6f6f6;
+   display: table-row;
+   background: #f6f6f6;
 }
 
 .row:nth-of-type(odd) {
-	background: #e9e9e9;
+   background: #e9e9e9;
 }
 
 .row.header {
-	font-weight: 900;
-	color: #ffffff;
-	background: #ea6153;
+   font-weight: 900;
+   color: #ffffff;
+   background: #ea6153;
 }
 
 .row.green {
-	background: #27ae60;
+   background: #27ae60;
 }
 
 .row.blue {
-	background: #2980b9;
+   background: #2980b9;
 }
 
 @media screen and (max-width: 580px) {
-	.row {
-		padding: 8px 0;
-		display: block;
-	}
+   .row {
+      padding: 8px 0;
+      display: block;
+   }
 }
 
 .cell {
-	padding: 6px 12px;
-	display: table-cell;
+   padding: 6px 12px;
+   display: table-cell;
 }
 
 @media screen and (max-width: 580px) {
-	.cell {
-		padding: 2px 12px;
-		display: block;
-	}
+   .cell {
+      padding: 2px 12px;
+      display: block;
+   }
 }
 </style>
 </head>
@@ -80,68 +80,63 @@
 
 
 
-	<div class="wrapper">
-		<h2>MY ACCOMPANY</h2>
-		<div class="table">
+   <div class="wrapper">
+      <h2>HOST GROUP</h2>
+      <div class="table">
 
-			<div class="row header">
-				<div class="cell">DATE</div>
-				<div class="cell">CITY</div>
-				<div class="cell">Current</div>
-				<div class="cell">Max</div>
-				<div class="cell">Detail</div>
-			</div>	
+         <div class="row header">
+            <div class="cell">DATE</div>
+            <div class="cell">CITY</div>
+            <div class="cell">Current</div>
+            <div class="cell">Max</div>
+            <div class="cell">Detail</div>
+         </div>   
 
-			<c:forEach items="${groupVO}" var="groupVO" >
-				<div class="row">
-					<div class="cell">${groupVO.tp_date}</div>
-					<div class="cell">${groupVO.cor_region}</div>
-					<div class="cell">${groupVO.current_num}</div>
-					<div class="cell">${groupVO.max_people_num}</div>
-					<div class="cell">
-						<strong><a
-							href='/mypage/read?c_num=${groupVO.accomp_group_num}'> Go </a></strong>
-					</div>
-				</div>
-			</c:forEach>
+         <c:forEach items="${groupVO}" var="groupVO" >
+            <div class="row">
+               <div class="cell">${groupVO.tp_date}</div>
+               <div class="cell">${groupVO.cor_region}</div>
+               <div class="cell">${groupVO.current_num}</div>
+               <div class="cell">${groupVO.max_people_num}</div>
+               <div class="cell">
+                  <strong><a
+                     href='/mypage/accom_read?group_id=${groupVO.accomp_group_num}'> Go </a></strong>
+               </div>
+            </div>
+         </c:forEach>
 
-		</div>
+      </div>
 
 
-		<h2>REQUEST accompany</h2>
-		<div class="table">
+      <h2>GUEST GROUP</h2>
+      <div class="table">
 
-			<div class="row header blue">
-				<div class="cell">Date</div>
-				<div class="cell">City</div>
-				<div class="cell">Current</div>
-				<div class="cell">Max</div>
-				<div class="cell">Detail</div>
-			</div>
+         <div class="row header blue">
+            <div class="cell">DATE</div>
+            <div class="cell">CITY</div>
+            <div class="cell">Current</div>
+            <div class="cell">Max</div>
+            <div class="cell">Detail</div>
+         </div>   
 
-			<c:forEach items="${requestList}" var="carpool">
-				<div class="row">
-					<div class="cell">
-						<img src="/resources/upload/${carpool.u_u_img }" width="30"
-							height="30"> ${carpool.c_u_name }
-					</div>
-					<div class="cell">${carpool.cr_ox}</div>
-					<div class="cell">${carpool.c_month },${carpool.c_date }th,
-						${carpool.c_year } ${carpool.c_hour }:${carpool.c_minute }</div>
-					<div class="cell">${carpool.start_point}</div>
-					<div class="cell">
-						<strong><a
-							href='/mypage/requestRead?cr_num=${carpool.cr_num }&c_num=${carpool.c_num}'>
-								Go </a></strong>
-					</div>
+         <c:forEach items="${groupVO2}" var="groupVO2" >
+            <div class="row">
+               <div class="cell">${groupVO2.tp_date}</div>
+               <div class="cell">${groupVO2.cor_region}</div>
+               <div class="cell">${groupVO2.current_num}</div>
+               <div class="cell">${groupVO2.max_people_num}</div>
+               <div class="cell">
+                  <strong><a
+                     href='/mypage/accom_read?group_id=${groupVO2.accomp_group_num}'> Go </a></strong>
+               </div>
+            </div>
+         </c:forEach>
 
-				</div>
-			</c:forEach>
-		</div>
+      </div>
 
-	</div>
+   </div>
 
-	<%@ include file="/WEB-INF/views/include/footer.jsp"%>
+   <%@ include file="/WEB-INF/views/include/footer.jsp"%>
 
 </body>
 </html>
